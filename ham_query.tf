a1e68edb91ab091ac3f1958973c6ecdfaca1464e
xss <michaela@michaela.lgbt>
2022-06-26 18:17:31 +1000

    increase telm memory

--- ham_query.tf
+++ ham_query.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "ham_telem" {
   s3_key           = aws_s3_bucket_object.lambda.key
   source_code_hash = data.archive_file.lambda.output_base64sha256
   publish          = true
-  memory_size      = 256
+  memory_size      = 1024
   role             = aws_iam_role.basic_lambda_role.arn
   runtime          = "python3.9"
   timeout          = 30
