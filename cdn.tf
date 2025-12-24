3882bac2852c9a7b88159cf895c68ece8a4f6603
xssfox <xss@sprocketfox.io>
2025-12-24 16:08:05 +1100

    aadd banner

--- cdn.tf
+++ cdn.tf
@@ -1014,6 +1014,36 @@ resource "aws_cloudfront_distribution" "api" {
     target_origin_id       = "Custom-api.${local.domain_name}"
     viewer_protocol_policy = "redirect-to-https"
   }
+
+  ordered_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT",
+    ]
+    cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    compress                 = true
+    default_ttl              = 0
+    max_ttl                  = 0
+    min_ttl                  = 0
+    origin_request_policy_id = "b689b0a8-53d0-40ab-baf2-68738e2966ac"
+    path_pattern             = "banner"
+    smooth_streaming         = false
+    target_origin_id         = "Custom-api.${local.domain_name}"
+    trusted_key_groups       = []
+    trusted_signers          = []
+    viewer_protocol_policy   = "redirect-to-https"
+  }
+
+
   comment     = ""
   price_class = "PriceClass_100"
   enabled     = true
