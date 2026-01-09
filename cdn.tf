8a44af36463e62c8620201c7d40b9debfb4fb288
darksidelemm <darksidelemm@users.noreply.github.com>
2026-01-09 06:46:10 +0000

    Terraform fmt [skip ci]

--- cdn.tf
+++ cdn.tf
@@ -1224,9 +1224,9 @@ resource "aws_s3_bucket_public_access_block" "skewt" {
 }
 
 resource "aws_s3_bucket_policy" "skewt" {
-  depends_on = [ aws_s3_bucket_public_access_block.skewt ]
-  bucket = aws_s3_bucket.skewt.id
-  policy = data.aws_iam_policy_document.skewt_public.json
+  depends_on = [aws_s3_bucket_public_access_block.skewt]
+  bucket     = aws_s3_bucket.skewt.id
+  policy     = data.aws_iam_policy_document.skewt_public.json
 }
 
 data "aws_iam_policy_document" "skewt_public" {
